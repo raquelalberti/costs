e38bdc5e4953f1109936910c9b174972b3faf1b8
raquelalberti <raquelalberti01@gmail.com>
2024-02-19 09:01:01 -0300

    full application

---
+++ src/components/layout/Loading.js
@@ -0,0 +1,12 @@
+import styles from './Loading.module.css'
+import loading from '../../img/loading.svg'
+
+function Loading() {
+    return (
+        <div className={styles.loader.container}>
+            <img className={styles.loader} src={loading} alt="Loading" />
+        </div>
+    )
+}
+
+export default Loading
